76579d2dc56d5ec91a822c3f30e230c8fd399dad
harun-wagura <harunndishu7@gmail.com>
2024-05-03 10:53:15 +0300

    Set the firebase key to be read from env variables

--- src/app.py
+++ src/app.py
@@ -10,10 +10,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firestore app
-# service_account_key =  os.environ.get('FIREBASE_CREDENTIALS_CERTIFICATE')
-# cred = credentials.Certificate(service_account_key)
-# print(cred)
-cred = credentials.Certificate("/serviceAccountKey.json")
+service_account_key =  os.environ.get('FIREBASE_CREDENTIALS_CERTIFICATE')
+cred = credentials.Certificate(service_account_key)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
