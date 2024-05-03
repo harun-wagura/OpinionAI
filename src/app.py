6663c2c86329da655c78fc1251295388ad25eb8f
harun wagura <harunndishu7@gmail.com>
2024-05-03 12:15:54 +0300

    Load firebase credentials from a .env file

--- src/app.py
+++ src/app.py
@@ -10,8 +10,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Initialize Firestore app
-service_account_key =  os.environ.get('FIREBASE_CREDENTIALS_CERTIFICATE')
-cred = credentials.Certificate(service_account_key)
+# service_account_key =  os.environ.get('FIREBASE_CREDENTIALS_CERTIFICATE')
+cred = credentials.Certificate("/etc/secrets/firebase.env")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
